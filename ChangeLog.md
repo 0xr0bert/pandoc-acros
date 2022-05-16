91b51d5d0b2d4a926d985802849f59cfd73339d7
Robert Greener <me@r0bert.dev>
2022-05-16 09:02:08 +0100

    Move project

--- ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for pandoc-acros
-
-## Unreleased changes
--- app/Main.hs
+++
@@ -1,7 +0,0 @@
-module Main where
-
-import Lib
-import Text.Pandoc.JSON ( toJSONFilter )
-
-main :: IO ()
-main = toJSONFilter processPandoc
